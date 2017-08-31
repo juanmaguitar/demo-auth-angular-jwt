5fc7243be0363332f4ac308b38c72963ec0177b3
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-08-31 17:07:52 +0200

    init angular

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 
 const passport = require('./config/passport/')
@@ -6,10 +7,13 @@ const passport = require('./config/passport/')
 const authRoutes = require('./routes/auth/')
 const privateRoutes = require('./routes/private/')
 
+const publicPath = path.join(__dirname, '../client')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use(express.static(publicPath))
 
 app.use(passport.initialize())
 
